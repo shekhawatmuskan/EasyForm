fe08c18d460c2281d0408c79bbd24abe72d5c77a
shekhawatmuskan <muskanshekhawat73@gmail.com>
2024-05-21 14:23:32 +0530

    updated form maker header

--- src/Components/FormMaker/FormMakerHeader/index.js
+++ src/Components/FormMaker/FormMakerHeader/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronLeft, faEye, faUpload, faTools, faPalette, faCode, faShareAlt, faPoll } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft, faEye, faUpload, faTools, faPalette, faCode, faShareAlt, faPoll } from '@fortawesome/free-solid-svg-icons';
 import './form-maker-header.css'; // Import the CSS file
 
 function FormMakerHeader() {
@@ -8,35 +8,44 @@ function FormMakerHeader() {
     <div className="form-header">
       <div className="form-header-content">
         {/* Arrow button with Font Awesome icon */}
-        <FontAwesomeIcon icon={faChevronLeft} className="arrow-icon" />
+        <button className="arrow-button blue">
+          <FontAwesomeIcon icon={faArrowLeft} className="button-icon" />
+        </button>
         <h1 className="form-title">My Form</h1>
       </div>
       <div className="action-buttons">
-        {/* Buttons with icons */}
+        {/* Buttons with icons and text */}
         <button className="action-button green">
-          <FontAwesomeIcon icon={faTools} className="button-icon" style={{ color: 'rgb(37 99 235 )' }} /> Build
+          <FontAwesomeIcon icon={faTools} className="button-icon" style={{ color: 'rgb(37 99 235 )' }} />
+          <span>Build</span>
         </button>
         <button className="action-button orange">
-          <FontAwesomeIcon icon={faPalette} className="button-icon" style={{ color: 'rgb(37 99 235 ' }} /> Design
+          <FontAwesomeIcon icon={faPalette} className="button-icon" style={{ color: 'rgb(37 99 235 ' }} />
+          <span>Design</span>
         </button>
         <button className="action-button blue">
-          <FontAwesomeIcon icon={faCode} className="button-icon" style={{ color: 'rgb(37 99 235 ' }} /> Integrate
+          <FontAwesomeIcon icon={faCode} className="button-icon" style={{ color: 'rgb(37 99 235 ' }} />
+          <span>Integrate</span>
         </button>
         <button className="action-button red">
-          <FontAwesomeIcon icon={faShareAlt} className="button-icon" style={{ color: 'rgb(37 99 235 ' }} /> Share
+          <FontAwesomeIcon icon={faShareAlt} className="button-icon" style={{ color: 'rgb(37 99 235 ' }} />
+          <span>Share</span>
         </button>
         <button className="action-button purple">
-          <FontAwesomeIcon icon={faPoll} className="button-icon" style={{ color: 'rgb(37 99 235 ' }} /> Results
+          <FontAwesomeIcon icon={faPoll} className="button-icon" style={{ color: 'rgb(37 99 235 ' }} />
+          <span>Results</span>
         </button>
       </div>
       <div className="action-buttons">
-        {/* Preview button with icon */}
+        {/* Preview button with icon and text */}
         <button className="preview-button">
-          <FontAwesomeIcon icon={faEye} className="button-icon" style={{ color: 'purple' }} /> Preview
+          <FontAwesomeIcon icon={faEye} className="button-icon" style={{ color: 'black' }} />
+          <span>Preview</span>
         </button>
         {/* Add a minor gap between Preview and Publish Now buttons */}
         <button className="publish-button">
-          <FontAwesomeIcon icon={faUpload} className="button-icon" style={{ color: 'purple' }} /> Publish Now
+          <FontAwesomeIcon icon={faUpload} className="button-icon" style={{ color: 'white' }} />
+          <span>Publish Now</span>
         </button>
       </div>
     </div>
